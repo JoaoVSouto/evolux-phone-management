cbe9f56750a94df190a7612a8a6da294805c89f9
JoaoVSouto <joaovitorss99@hotmail.com>
2021-03-21 12:10:46 -0300

    Update toast styles

--- src/components/ToastContainer.js
+++ src/components/ToastContainer.js
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { FaCheckCircle } from 'react-icons/fa';
+
 import Toast from 'react-bootstrap/Toast';
 
 import { removeToast } from '../ducks/toastsSlice';
@@ -18,18 +19,20 @@ function ToastContainer() {
       {toasts.map(toast => (
         <Toast
           key={toast.id}
-          className="bg-white"
+          className="toast-container__toast"
           autohide
           delay={3000}
           onClose={() => handleToastClose(toast.id)}
         >
-          <Toast.Header>
-            <strong className="mr-auto text-success d-flex align-items-center">
-              <FaCheckCircle className="mr-2" />
+          <Toast.Header className="toast-container__toast__header">
+            <strong className="toast-container__toast__title">
+              <FaCheckCircle className="toast-container__toast__icon" />
               {toast.title}
             </strong>
           </Toast.Header>
-          <Toast.Body>{toast.description}</Toast.Body>
+          <Toast.Body className="toast-container__toast__body">
+            {toast.description}
+          </Toast.Body>
         </Toast>
       ))}
     </div>
