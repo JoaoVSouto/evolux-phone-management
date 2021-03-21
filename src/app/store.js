f36879f9a9d3f1539a710041c847d0cb011d5958
JoaoVSouto <joaovitorss99@hotmail.com>
2021-03-21 14:15:16 -0300

    Configure dids slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import toastsSlice from '../ducks/toastsSlice';
+import didsSlice from '../ducks/didsSlice';
 
 export default configureStore({
   reducer: {
     toasts: toastsSlice,
+    dids: didsSlice,
   },
 });
---
+++ src/ducks/didsSlice.js
@@ -0,0 +1,49 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+import api from '../services/api';
+
+const DIDS_PER_PAGE = 20;
+
+export const didsSlice = createSlice({
+  name: 'dids',
+  initialState: {
+    isLoading: false,
+    hasError: false,
+    items: [],
+  },
+  reducers: {
+    setDidsLoading(state) {
+      state.hasError = false;
+      state.isLoading = true;
+    },
+    setDidsSuccess(state, action) {
+      state.hasError = false;
+      state.isLoading = false;
+      state.items = action.payload;
+    },
+    setDidsError(state) {
+      state.hasError = true;
+      state.isLoading = false;
+    },
+  },
+});
+
+const { setDidsLoading, setDidsSuccess, setDidsError } = didsSlice.actions;
+
+export const fetchDids = ({ page = 1 }) => async dispatch => {
+  dispatch(setDidsLoading());
+
+  try {
+    const { data } = await api.get('dids', {
+      params: {
+        _limit: DIDS_PER_PAGE,
+        _page: page,
+      },
+    });
+    dispatch(setDidsSuccess(data));
+  } catch {
+    dispatch(setDidsError());
+  }
+};
+
+export default didsSlice.reducer;
