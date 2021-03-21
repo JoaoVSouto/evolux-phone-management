0997de481515855252a89fb6806e408abb9b68dc
JoaoVSouto <joaovitorss99@hotmail.com>
2021-03-20 21:14:21 -0300

    Configure toasts slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import toastsSlice from '../ducks/toastsSlice';
+
 export default configureStore({
-  reducer: {},
+  reducer: {
+    toasts: toastsSlice,
+  },
 });
---
+++ src/ducks/toastsSlice.js
@@ -0,0 +1,35 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { v4 as uuid } from 'uuid';
+
+export const toastsSlice = createSlice({
+  name: 'toasts',
+  initialState: {
+    toasts: [],
+  },
+  reducers: {
+    addToast(state, action) {
+      const toast = action.payload;
+      state.toasts.push(toast);
+    },
+    removeToast(state, action) {
+      const toastId = action.payload;
+      state.toasts = state.toasts.filter(toast => toast.id !== toastId);
+    },
+  },
+});
+
+export const { removeToast } = toastsSlice.actions;
+
+export const addToast = toastData => dispatch => {
+  const { title, description } = toastData;
+
+  dispatch(
+    toastsSlice.actions.addToast({
+      id: uuid(),
+      title,
+      description,
+    })
+  );
+};
+
+export default toastsSlice.reducer;
