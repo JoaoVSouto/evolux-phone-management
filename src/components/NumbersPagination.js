baab32831d82494462eb5aacd9a2118bfbbcc4fc
JoaoVSouto <joaovitorss99@hotmail.com>
2021-03-22 22:04:24 -0300

    Create pagination buttons logic

--- src/components/NumbersPagination.js
+++ src/components/NumbersPagination.js
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { useSelector } from 'react-redux';
 
 import Pagination from 'react-bootstrap/Pagination';
@@ -6,20 +7,62 @@ import { hasPagination as hasPaginationSelector } from '../ducks/didsSlice';
 
 function NumbersPagination() {
   const hasPagination = useSelector(state => hasPaginationSelector(state.dids));
+  const { currentPage, lastPage } = useSelector(state => state.dids);
+
+  const isFirstPage = React.useMemo(() => currentPage === 1, [currentPage]);
+  const isLastPage = React.useMemo(() => currentPage === lastPage, [
+    currentPage,
+    lastPage,
+  ]);
+
+  if (!hasPagination) {
+    return null;
+  }
 
   return (
-    hasPagination && (
-      <Pagination>
-        <Pagination.First disabled />
-        <Pagination.Prev disabled />
-        <Pagination.Item active>1</Pagination.Item>
-        <Pagination.Item>2</Pagination.Item>
+    <Pagination className="justify-content-center">
+      <Pagination.First disabled={isFirstPage} />
+      <Pagination.Prev disabled={isFirstPage} />
+      <Pagination.Item active={isFirstPage}>1</Pagination.Item>
+      {isFirstPage && lastPage > 2 && <Pagination.Item>2</Pagination.Item>}
+      {isFirstPage && lastPage > 3 && <Pagination.Item>3</Pagination.Item>}
+      {currentPage === 2 && lastPage > 2 && (
+        <Pagination.Item active>2</Pagination.Item>
+      )}
+      {currentPage === 2 && lastPage > 3 && (
+        <Pagination.Item>3</Pagination.Item>
+      )}
+
+      {currentPage > 3 && lastPage > 4 && <Pagination.Ellipsis disabled />}
+
+      {currentPage > 2 && currentPage < lastPage - 1 && (
+        <>
+          <Pagination.Item>{currentPage - 1}</Pagination.Item>
+          <Pagination.Item active>{currentPage}</Pagination.Item>
+          <Pagination.Item>{currentPage + 1}</Pagination.Item>
+        </>
+      )}
+
+      {currentPage < lastPage - 2 && lastPage > 4 && (
         <Pagination.Ellipsis disabled />
-        <Pagination.Item>40</Pagination.Item>
-        <Pagination.Next />
-        <Pagination.Last />
-      </Pagination>
-    )
+      )}
+
+      {currentPage === lastPage - 1 && currentPage > 2 && (
+        <Pagination.Item>{currentPage - 1}</Pagination.Item>
+      )}
+      {currentPage === lastPage - 1 && currentPage > 2 && (
+        <Pagination.Item active>{currentPage}</Pagination.Item>
+      )}
+      {isLastPage && lastPage - 2 > 1 && (
+        <Pagination.Item>{lastPage - 2}</Pagination.Item>
+      )}
+      {isLastPage && lastPage - 1 > 1 && (
+        <Pagination.Item>{lastPage - 1}</Pagination.Item>
+      )}
+      <Pagination.Item active={isLastPage}>{lastPage}</Pagination.Item>
+      <Pagination.Next disabled={isLastPage} />
+      <Pagination.Last disabled={isLastPage} />
+    </Pagination>
   );
 }
 
