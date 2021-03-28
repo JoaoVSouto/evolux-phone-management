83b53104a0d20c262204a6404a9802e85683f16b
JoaoVSouto <joaovitorss99@hotmail.com>
2021-03-28 19:33:28 -0300

    Dispatch did fetching when number is searched

--- src/components/NumbersSearch.js
+++ src/components/NumbersSearch.js
@@ -1,8 +1,22 @@
 import FormControl from 'react-bootstrap/FormControl';
 
+import useDebounce from '~/hooks/useDebounce';
+import useFetchDids from '~/hooks/useFetchDids';
+
 function NumbersSearch() {
+  const fetchDids = useFetchDids();
+
+  const handleInputChange = e => {
+    fetchDids({
+      query: e.target.value,
+    });
+  };
+
+  const debouncedHandleInputChange = useDebounce(handleInputChange, 500);
+
   return (
     <FormControl
+      onChange={debouncedHandleInputChange}
       className="numbers-search order-1 order-sm-0 mt-3 mt-sm-0"
       placeholder="Search DIDs by number"
     />
